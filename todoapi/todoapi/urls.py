06ae3215d839bdec8e9cf072062a0e001067ec91
sabretooth1405 <sabretooth1405@gmail.com>
2023-03-01 15:49:51 +0530

    corrected paths

--- todoapi/todoapi/urls.py
+++ todoapi/todoapi/urls.py
@@ -5,5 +5,5 @@ from .views import home
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('api.urls')),
-    path("",home,name="home")
+    path("/",home,name="home")
 ]
